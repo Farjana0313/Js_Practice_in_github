f8016cf5f99f94627f540c765953eea31977b967
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2024-01-05 12:33:38 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = 'farjana@gmail.com'
+var accountPassword = "12345"
+accountCtiy = "Dhaka";
+var accountSate;
+
+
+accountEmail = "test@gmail.com";
+accountPassword = "54321";
+
+
+console.log(accountId);
+
+/*
+Prefer Not to use var
+becasue of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCtiy, accountSate])
