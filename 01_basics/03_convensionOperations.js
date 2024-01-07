2e51aeec4e5688b9435d35f0820336c4d44de5f3
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2024-01-07 03:33:29 +0000

    update convesion operation

--- 01_basics/03_convensionOperations.js
+++ 01_basics/03_convensionOperations.js
@@ -28,3 +28,32 @@ let stringNumber = String(someNumber)
 
 // *********************** Operations ***********************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = " hello"
+let str2 = "farjana"
+let srt3 = str2 + str1
+// console.log(srt3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+// console.log(num1, num2, num3);
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
