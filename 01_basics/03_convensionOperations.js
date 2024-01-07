49644e1f9dee08ef3ac7b968c25e3d2fbe36a059
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2024-01-07 03:14:18 +0000

    converstion operations

---
+++ 01_basics/03_convensionOperations.js
@@ -0,0 +1,30 @@
+let score = "farjana"
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+/* Notes
+
+"33" => 33
+"33abc" => Nan
+true => 1; fakse=>0
+*/
+
+
+let isLoggedIn = "farjana"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+// "" => false
+// "farjana" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
