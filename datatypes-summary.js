4a563687d66a890b66519b5c6936eadff200a74a
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2024-01-07 04:34:03 +0000

    datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,15 @@
+//  #Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbole, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let useEmail;
+
+// Symbole
+const mySymbol = Symbol("my symbol");
+const id = Symbol('123')
+
+// Reference Type (Non primitive)
+// Array, Objects, Functions    
