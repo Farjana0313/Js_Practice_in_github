b8a2afa04daf9d14d771f247bc5b051c8c50ccbb
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2024-01-07 07:16:56 +0000

    datatypes update

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -10,6 +10,29 @@ let useEmail;
 // Symbole
 const mySymbol = Symbol("my symbol");
 const id = Symbol('123')
+const anotheriId = Symbol('123')
+
+// console.log(id === anotheriId);
+// BigInt
+const bigNumber = 9007199254740991n;
+// console.log(bigNumber);
+
 
 // Reference Type (Non primitive)
 // Array, Objects, Functions    
+
+const heros = ["saktiman", "naagarraj", "doga"];
+console.log(heros[0]);
+const user = {
+    name: 'John',
+    age: 22
+}
+
+const myFunction = function () {
+    console.log("hello world");
+}
+
+myFunction();
+
+
+console.log(typeof myFunction);
